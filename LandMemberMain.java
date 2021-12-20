7aa56a64a92290de1456262e3de835b1bbf4abfb
Choi_J_I <95062692+Shaharen@users.noreply.github.com>
2021-12-20 14:10:01 +0900

    Update LandMemberMain.java

--- LandMemberMain.java
+++ LandMemberMain.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 import javax.print.DocFlavor.STRING;
 import javazoom.jl.player.MP3Player; 
 
-public class LandMemberMain {
+public class LandMemberMain { 
 
 	public static void main(String[] args) {
 
